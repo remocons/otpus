1a9e109546233eb81974706cf720599695772c46
Lee Dong Eun <sixgen@gmail.com>
2022-04-09 22:46:47 +0900

    change  encryptMessage hmac

--- src/otpus.js
+++ src/otpus.js
@@ -18,13 +18,12 @@ export function RAND(size){
  * @param {Uint8Array} data 
  * @param {Uint8Array} otpKey32Bytes 
  * @param {Number} otpStartIndex 
- * @param {boolean} shareDataBuffer  true: modiry origin data,  false: return new data.
+ * @param {boolean} shareDataBuffer  true: modify origin data,  false: return new data.
  * @returns encryptedData
  */
 export function xotp(data, otpKey32Bytes, otpStartIndex = 0, shareDataBuffer = false) {
 
-    if (!(otpKey32Bytes instanceof Uint8Array && otpKey32Bytes.byteLength === 32)) {
-        console.log('wrong type of otpKey32Bytes:', otpKey32Bytes)
+    if (!(otpKey32Bytes instanceof Uint8Array) || !(otpKey32Bytes.byteLength === 32) ) {
         throw new TypeError('xotp: Use 32 byteLength Uint8Array key.')
     }
 
@@ -32,6 +31,13 @@ export function xotp(data, otpKey32Bytes, otpStartIndex = 0, shareDataBuffer = f
         throw new TypeError('xotp:  Use Uint8Array data. ')
     }
 
+    if ( typeof otpStartIndex !== 'number' ) {
+        throw new TypeError('otpStartIndex:  Use Number. 0 ~  2 ** 32 - 1')
+    }
+
+    if ( typeof shareDataBuffer !== 'boolean' ) {
+        throw new TypeError('sharedDataBuffer: Use boolean. true or false.')
+    }
     // otpKey is 36bytes  
     // 32Bytes otpKey32Bytes + 4byte Uint32 Counter( LittleEndian ). (like Uint32Array(value ) )
     const otpKey = MBP.U8pack(otpKey32Bytes, MBP.NB('32L', otpStartIndex))
@@ -81,23 +87,28 @@ export function xotp(data, otpKey32Bytes, otpStartIndex = 0, shareDataBuffer = f
  * 
  * @param {String} msg plainText
  * @param {String} key passPhrase
- * @param {Number} nPower keyAging factor.  default 10 => 2**10 => repeat hash 1024 times.
+ * @param {Number} nPower keyAging factor for nTimesHash().  default 10 => 2 ** 10 => repeat hash 1024 times.
  * @returns 
  */
 export function encryptMessage(msg, key, nPower = 10) {
 
+
+    if ( typeof msg !== 'string' ) {
+        throw new TypeError('msg: Use string message.')
+    }
     const msgBuffer = MBP.U8(msg) 
     const salt = RAND(32)
     const saltStr = buf2hex(salt)
     const mainKey = nTimesHash(saltStr + key , 2 ** nPower )
     const randSize = parseInt( RAND(1)[0] / 4 )  // random number range  0~63
     const randBuffer = RAND( randSize )
-    const hmac = sha256.hmac( mainKey , msgBuffer)
 
     const randomSizeMessage = MBP.pack(
         MBP.MB('#rand', randBuffer),
         MBP.MB('msg', msgBuffer )
     )
+
+    const hmac = sha256.hmac( mainKey , randomSizeMessage)
 
     const encMsg = xotp( randomSizeMessage , mainKey  )
 
@@ -111,6 +122,8 @@ export function encryptMessage(msg, key, nPower = 10) {
     return pack.toString('base64')
 }
 
+// return string if success.
+// return undefined if fail.
 export function decryptMessage(b64msg, key) {
     const msgObj = MBP.unpack(Buffer.from(b64msg, 'base64'))
     // console.log( 'msgObj', msgObj)
@@ -123,24 +136,18 @@ export function decryptMessage(b64msg, key) {
     const mainKey = nTimesHash(saltStr + key, 2 ** msgObj.nPower )
 
     const decMsg = xotp(msgObj.encMsg, mainKey, 0)
+    const hmac = sha256.hmac(mainKey, decMsg)
 
     try {
         const pack = MBP.unpack( decMsg )
-
         if( typeof pack === 'object'){
-            console.log( pack )
-            const hmac = sha256.hmac(mainKey, pack.msg)
+            // console.log( pack )
             if (!equal(hmac, msgObj.hmac)) return  //'Wrong HMAC: BROKEN'
             const msg = decoder.decode(pack.msg)
             return msg
         }
     } catch (error) {
-        
     }
-
-    return 
-
-
 }
 
 
